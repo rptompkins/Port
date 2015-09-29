2553d6ef4dbf194db808e31be2a8b10854987fcc
rptompkins <ripoto3@gmail.com>
2015-09-29 10:14:08 -0700

    match local server

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 200) {
+    if ($(".navbar").offset().top > 670) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
         $(".logo").addClass("logo-fixed");
     } else {
@@ -199,19 +199,5 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
 }
